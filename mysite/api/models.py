ff7551b733a138f008207f48487be607188b3613
trailtony <faithmakerneo@gmail.com>
2025-12-04 12:54:48 +0000

    Added BlogPost model classto models.py and BlogPostSerializer to serializer.py

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_add_now=True)
+
+    def __str__(self):
+        return self.title
