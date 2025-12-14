027fe2e7f183f10ef80e287c46246f234d887a82
trailtony <still_running_uphill@protonmail.com>
2025-12-14 20:25:39 +0000

    Added blog post retireval, update and destroy functionalities to views.

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import BlogPost
 from .serializer import BlogPostSerializer
 
 # Create your views here.
 class BlogPostListCreate(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
-    serializer_class = BlogPostSerializer 
+    serializer_class = BlogPostSerializer
+    
+    def delete(self, request, *args, **kwargs):
+        BlogPost.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class BlogPostRetrieveUpdateDestory(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    lookup_field = "pk"
