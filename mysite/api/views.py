865b54f26156cb64342de58be8e1e641e6d66a17
trailtony <still_running_uphill@protonmail.com>
2025-12-16 23:53:41 +0000

    Added custom API view to views.

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.response import Response
 from .models import BlogPost
-from .serializer import BlogPostSerializer
+from .serializers import BlogPostSerializer
+from rest_framework.views import APIView
 
 # Create your views here.
 class BlogPostListCreate(generics.ListCreateAPIView):
@@ -18,3 +19,15 @@ class BlogPostRetrieveUpdateDestory(generics.RetrieveUpdateDestroyAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
     lookup_field = "pk"
+    
+class BlogPostList(APIView):
+    def get(self, request, format=None):
+        title = request.query_params.get('title', "")
+        
+        if title:
+            blog_posts = BlogPost.objects.filter(title__icontains=title)
+        else:
+            blog_posts = BlogPost.objects.all()
+        
+        serializer = BlogPostSerializer(blog_posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
